af6237c5d0fddbb2dde2a52366fa721e4bf53c1e
Md. Alif Khan Rifat <73586056+mdalifkhanrifat@users.noreply.github.com>
2020-11-11 01:09:31 +0600

    Update if-else.py

--- HackerRank/if-else.py
+++ HackerRank/if-else.py
@@ -1,1 +1,21 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+def display(x):
+    if x%2==0:
+        if 2<=n<=5 or n>20:
+            print("Not Weird")
+        elif 6<=n<=20:
+            print("Weird")  
+    else:
+        print("Weird")
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    display(n)
